f1ec2797950d1ad9ebb237960300db49d7b33dac
kenmasters <kjsiosan@gmail.com>
2019-01-31 02:57:51 -0500

    Update default test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->visit($this->baseUrl)
+             ->see('Team MaMaasin');
     }
 }
